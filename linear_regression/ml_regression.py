015af6a667343596a136e755cef077c1d27d783c
sjriddle <samuel.riddle1@gmail.com>
2018-08-16 18:00:44 -0600

    Added r_squared and edited linear regression

--- linear_regression/ml_regression.py
+++ linear_regression/ml_regression.py
@@ -17,15 +17,15 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 x = np.array(df.drop(['label'],1))
 x = preprocessing.scale(x)
-x = x[:-forecast_out]
 x_lately = x[-forecast_out:]
+x = x[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
