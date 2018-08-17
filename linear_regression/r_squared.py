72766d4de3cf6232a7ad631f960e24f4dec93db2
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-08-17 08:21:44 -0600

    Update r_squared.py

--- linear_regression/r_squared.py
+++ linear_regression/r_squared.py
@@ -6,8 +6,8 @@ import random
 
 style.use('fivethirtyeight')
 
-#xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-#ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
 def create_dataset(hm, variance, step=2, correlation=False):
     val = 1
@@ -20,12 +20,14 @@ def create_dataset(hm, variance, step=2, correlation=False):
         elif correlation and correlation == 'neg':
             val -= step
     xs = [i for i in range(len(ys))]
+    
     return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
          ((mean(xs) * mean(xs)) - mean(xs*xs)))
     b = mean(ys) - m*mean(xs)
+    
     return m,b
 
 def squared_error(ys_orig, ys_line):
@@ -35,6 +37,7 @@ def coefficient_of_determination(ys_orig, ys_line):
     y_mean_line = [mean(ys_orig) for y in ys_orig]
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    
     return 1 - (squared_error_regr / squared_error_y_mean)
 
 xs, ys = create_dataset(40, 10, 2, correlation='pos')
@@ -44,7 +47,6 @@ regression_line = [(m*x)+b for x in xs]
 
 predict_x = 8
 predict_y = (m*predict_x) + b
-
 r_squared = coefficient_of_determination(ys, regression_line)
 print(r_squared)
 
