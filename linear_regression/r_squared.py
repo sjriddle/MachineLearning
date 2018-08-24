c2f788cdf72983def70bad79fb8dfb6d7275512a
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-08-24 08:28:13 -0600

    Update r_squared.py

--- linear_regression/r_squared.py
+++ linear_regression/r_squared.py
@@ -22,23 +22,26 @@ def create_dataset(hm, variance, step=2, correlation=False):
     xs = [i for i in range(len(ys))]
     return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
+
 def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
          ((mean(xs) * mean(xs)) - mean(xs*xs)))
     b = mean(ys) - m*mean(xs)
     return m,b
 
+
 def squared_error(ys_orig, ys_line):
     return sum((ys_line - ys_orig)**2)
-    
+
+
 def coefficient_of_determination(ys_orig, ys_line):
     y_mean_line = [mean(ys_orig) for y in ys_orig]
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
 
-xs, ys = create_dataset(40, 10, 2, correlation='pos')
 
+xs, ys = create_dataset(40, 10, 2, correlation='pos')
 m,b = best_fit_slope_and_intercept(xs,ys)
 regression_line = [(m*x)+b for x in xs]
 
