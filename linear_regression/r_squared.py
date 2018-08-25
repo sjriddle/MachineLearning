bc0eef4465158e7ec58c938a75453d2471b403e8
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-08-25 13:03:33 -0600

    Update r_squared.py

--- linear_regression/r_squared.py
+++ linear_regression/r_squared.py
@@ -20,6 +20,7 @@ def create_dataset(hm, variance, step=2, correlation=False):
         elif correlation and correlation == 'neg':
             val -= step
     xs = [i for i in range(len(ys))]
+    
     return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 
@@ -27,6 +28,7 @@ def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
          ((mean(xs) * mean(xs)) - mean(xs*xs)))
     b = mean(ys) - m*mean(xs)
+    
     return m,b
 
 
