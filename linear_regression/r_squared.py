b23609b83c640f2f6cbc77f0c0d05e2f3e120cf8
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-10-03 09:18:25 -0600

    Update r_squared.py

--- linear_regression/r_squared.py
+++ linear_regression/r_squared.py
@@ -28,7 +28,6 @@ def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
          ((mean(xs) * mean(xs)) - mean(xs*xs)))
     b = mean(ys) - m*mean(xs)
-    
     return m,b
 
 
