a1c6f484c33a93e9371c5193c911f5ade99fe0b5
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-05-04 21:46:01 -0600

    Update sentiment_neural_network.py

--- sentiment_featuresets/sentiment_neural_network.py
+++ sentiment_featuresets/sentiment_neural_network.py
@@ -42,7 +42,6 @@ def train_neural_network(x):
     prediction = neural_network_model(x)
     cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    
     hm_epochs = 10
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer()) 
@@ -57,7 +56,7 @@ def train_neural_network(x):
                 _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
                 epoch_loss += c
                 i += batch_size
-            print('Epoch', epoch+1, 'completed out of',hm_epochs,'loss:',epoch_loss)
+            print('Epoch ', epoch+1, ' completed out of ', hm_epochs, 'loss:',epoch_loss)
             
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
