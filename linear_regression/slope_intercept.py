9ce1440036659ef118f338113fe3fda067356b13
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-05-12 10:39:43 -0600

    Update slope_intercept.py

--- linear_regression/slope_intercept.py
+++ linear_regression/slope_intercept.py
@@ -8,8 +8,7 @@ xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs,ys):
-    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
-         ((mean(xs) * mean(xs)) - mean(xs*xs)))
+    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / ((mean(xs) * mean(xs)) - mean(xs*xs)))
     b = mean(ys) - m * mean(xs)
     return m,b
 
