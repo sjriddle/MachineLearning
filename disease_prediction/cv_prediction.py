f97bfb844327860f1214021bda5dabe904a08279
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-10-28 14:49:20 -0600

    Update cv_prediction.py

--- disease_prediction/cv_prediction.py
+++ disease_prediction/cv_prediction.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns  # plot
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
