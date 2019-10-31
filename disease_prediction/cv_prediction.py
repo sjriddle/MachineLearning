556f84c2d0eefd2a760e99549f9447e8fe57bfc6
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-10-31 08:36:00 -0600

    Update cv_prediction.py

--- disease_prediction/cv_prediction.py
+++ disease_prediction/cv_prediction.py
@@ -15,50 +15,52 @@ warnings.filterwarnings('ignore')
 df = pd.read_csv('./disease_prediction/cv_train.csv', sep=';')
 df.head()
 
-df.drop('id', axis=1, inplace=True)
-df.info()
+plt.show()
 
-df.describe()
-sns.countplot(x='cardio', data=df, hue='gender', palette='rainbow')
+# df.drop('id', axis=1, inplace=True)
+# df.info()
 
-plt.figure(figsize=(14,6))
-plt.subplot(1,2,1)
-sns.boxplot(x='cardio', y='height', data=df, palette='winter')
-plt.subplot(1,2,2)
-sns.boxplot(x='cardio', y='weight', data=df, palette='summer')
+# df.describe()
+# sns.countplot(x='cardio', data=df, hue='gender', palette='rainbow')
 
-correlations = df.corr()['cardio'].drop('cardio')
-print(correlations)
+# plt.figure(figsize=(14,6))
+# plt.subplot(1,2,1)
+# sns.boxplot(x='cardio', y='height', data=df, palette='winter')
+# plt.subplot(1,2,2)
+# sns.boxplot(x='cardio', y='weight', data=df, palette='summer')
 
-
-def feat_select(threshold):
-    abs_cor = correlations.abs()
-    features = abs_cor[abs_cor > threshold].index.tolist()
-    return features
+# correlations = df.corr()['cardio'].drop('cardio')
+# print(correlations)
 
 
-def model(mod,X_tr,X_te):
-    mod.fit(X_tr,y_train)
-    pred = mod.predict(X_te)
-    print('Model score = ',mod.score(X_te,y_test)*100,'%')
+# def feat_select(threshold):
+#     abs_cor = correlations.abs()
+#     features = abs_cor[abs_cor > threshold].index.tolist()
+#     return features
 
 
-msk = np.random.rand(len(df))<0.85
-df_train_test = df[msk]
-df_val = df[~msk]
+# def model(mod,X_tr,X_te):
+#     mod.fit(X_tr,y_train)
+#     pred = mod.predict(X_te)
+#     print('Model score = ',mod.score(X_te,y_test)*100,'%')
 
-X = df_train_test.drop('cardio',axis=1)
-y = df_train_test['cardio']
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=70)
 
-lr = LogisticRegression()
-threshold = [0.001,0.002,0.005,0.01,0.05,0.1]
-for i in threshold:
-    print('\n',i)
-    feature_i = feat_select(i)
-    X_train_i = X_train[feature_i]
-    X_test_i = X_test[feature_i]
-    model(lr,X_train_i,X_test_i)
+# msk = np.random.rand(len(df))<0.85
+# df_train_test = df[msk]
+# df_val = df[~msk]
+
+# X = df_train_test.drop('cardio',axis=1)
+# y = df_train_test['cardio']
+# X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=70)
+
+# lr = LogisticRegression()
+# threshold = [0.001,0.002,0.005,0.01,0.05,0.1]
+# for i in threshold:
+#     print('\n',i)
+#     feature_i = feat_select(i)
+#     X_train_i = X_train[feature_i]
+#     X_test_i = X_test[feature_i]
+#     model(lr,X_train_i,X_test_i)
 
 
 
