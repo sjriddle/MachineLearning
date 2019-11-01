85d8227eaaf375bcb94c24ade07543b37cbfb6cd
Samuel Riddle <riddle@adobe.com>
2019-11-01 09:02:31 -0600

    Changed some of the prediction cv

--- disease_prediction/cv_prediction.py
+++ disease_prediction/cv_prediction.py
@@ -15,13 +15,11 @@ warnings.filterwarnings('ignore')
 df = pd.read_csv('./cv_train.csv', sep=';')
 df.head()
 
-# df.drop('id', axis=1, inplace=True)
-# df.info()
-
-# df.describe()
-sns.countplot(x='cardio', data=df, hue='gender', palette='rainbow')
+df.drop('id', axis=1, inplace=True)
+sns.countplot(x='cardio', data=df, hue='gender')
 plt.show()
 
+# Finds values that have strong correlations with the diagnoses of CV disease
 correlations = df.corr()['cardio'].drop('cardio')
 print(correlations)
 
@@ -32,83 +30,26 @@ def feat_select(threshold):
     return features
 
 
-def model(mod,X_tr,X_te):
+def model(mod, X_tr, X_te):
     mod.fit(X_tr,y_train)
     pred = mod.predict(X_te)
-    print('Model score = ',mod.score(X_te,y_test)*100,'%')
+    print('Model score = ', mod.score(X_te,y_test)*100, '%')
 
 
 # split data
-msk = np.random.rand(len(df))<0.85
+msk = np.random.rand(len(df)) < 0.85
 df_train_test = df[msk]
 df_val = df[~msk]
 
-X = df_train_test.drop('cardio',axis=1)
+X = df_train_test.drop('cardio', axis=1)
 y = df_train_test['cardio']
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=70)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=70)
 
 lr = LogisticRegression()
-threshold = [0.001,0.002,0.005,0.01,0.05,0.1]
+threshold = [0.001, 0.002, 0.005, 0.01, 0.05, 0.1]
 for i in threshold:
-    print('\n',i)
+    print('\n', i)
     feature_i = feat_select(i)
     X_train_i = X_train[feature_i]
     X_test_i = X_test[feature_i]
     model(lr,X_train_i,X_test_i)
-
-
-scale = StandardScaler()
-scale.fit(X_train)
-X_train_scaled = scale.transform(X_train)
-X_train_ = pd.DataFrame(X_train_scaled,columns=df.columns[:-1])
-
-scale.fit(X_test)
-X_test_scaled = scale.transform(X_test)
-X_test_ = pd.DataFrame(X_test_scaled,columns=df.columns[:-1])
-
-# optimum k with optimum threshold
-for i in threshold:
-    feature = feat_select(i)
-    X_train_k = X_train_[feature]
-    X_test_k = X_test_[feature]
-    err = []
-    for j in range(1,30):
-        knn = KNeighborsClassifier(n_neighbors=j)
-        knn.fit(X_train_k,y_train)
-        pred_j = knn.predict(X_test_k)
-        err.append(np.mean(y_test != pred_j))
-
-    plt.figure(figsize=(10,6))
-    plt.plot(range(1,30),err)
-    plt.xlabel('K value')
-    plt.ylabel('Error')
-
-# final feature selection with threshold 0.05
-feat_final = feat_select(0.05)
-print(feat_final)
-
-# scaling the val data as well
-X_train = X_train_[feat_final]
-X_val = np.asanyarray(df_val[feat_final])
-y_val = np.asanyarray(df_val['cardio'])
-
-scale.fit(X_val)
-X_val_scaled = scale.transform(X_val)
-X_val_ = pd.DataFrame(X_val_scaled,columns=df_val[feat_final].columns)
-
-# knn with k=15
-knn = KNeighborsClassifier(n_neighbors=15)
-knn.fit(X_train,y_train)
-pred = knn.predict(X_val_)
-
-# reports
-print('Confusion Matrix =\n',confusion_matrix(y_val,pred))
-print('\n',classification_report(y_val,pred))
-
-# Logistic regression
-lr.fit(X_train,y_train)
-pred = lr.predict(X_val_)
-
-# reports
-print('Confusion Matrix =\n',confusion_matrix(y_val,pred))
-print('\n',classification_report(y_val,pred))
