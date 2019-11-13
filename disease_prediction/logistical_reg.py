1effbc0534ca7eae36ffbe60871b5448111c7bbd
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-11-13 13:50:53 -0700

    Update logistical_reg.py

--- disease_prediction/logistical_reg.py
+++ disease_prediction/logistical_reg.py
@@ -25,6 +25,7 @@ x = df_train_test.drop('cardio',axis=1)
 y = df_train_test['cardio']
 x_train, x_score, y_train, y_score = train_test_split(x, y, test_size=0.15, random_state=75)
 
+# Declare Logistic Regression from sklearn
 lr = LogisticRegression()
 
 # Function that fits and scores a specific model
@@ -35,7 +36,7 @@ def train_model(model, x_tr, x_te):
 # Declare different thrsholds and output model score
 th = [0.001, 0.005, 0.01, 0.05, 0.1]
 for i in th:
-    print('\n', 'Given Threshold: ', float(i * 100), '%')
+    print('\n', 'Model Threshold: ', float(i * 100), '%')
     attr_i = cardio_corr.abs()[cardio_corr.abs() > i].index.tolist()
     x_train_i = x_train[attr_i]
     x_score_i = x_score[attr_i]
