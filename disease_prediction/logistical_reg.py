3f5c8cee049435c3b1b2181df2d4c5739c07b715
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-11-20 10:50:57 -0700

    Update logistical_reg.py

--- disease_prediction/logistical_reg.py
+++ disease_prediction/logistical_reg.py
@@ -1,8 +1,8 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import warnings
 
 warnings.filterwarnings("ignore")
